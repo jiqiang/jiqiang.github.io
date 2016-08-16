f24b75da8c58d3b48ae3c21e91b3f47924120da8
Qiang Ji <qiang.ji@yahoo.com.au>
2016-08-16 23:02:25 +1000

    Add template.

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+
+var javascript = new Chart(document.getElementById("javascript").getContext("2d"))
+  .Doughnut([
+    {
+      value: 70,
+      color:"#1abc9c"
+    },
+    {
+      value : 30,
+      color : "#ecf0f1"
+    }
+  ]);
+
+var bootstrap = new Chart(document.getElementById("bootstrap").getContext("2d"))
+  .Doughnut([
+    {
+      value: 90,
+      color:"#1abc9c"
+    },
+    {
+      value : 10,
+      color : "#ecf0f1"
+    }
+  ]);
+
+var wordpress = new Chart(document.getElementById("wordpress").getContext("2d"))
+  .Doughnut([
+    {
+      value: 65,
+      color:"#1abc9c"
+    },
+    {
+      value : 35,
+      color : "#ecf0f1"
+    }
+  ]);
+
+var htmlcss = new Chart(document.getElementById("html").getContext("2d"))
+  .Doughnut([
+    {
+      value: 80,
+      color:"#1abc9c"
+    },
+    {
+      value : 20,
+      color : "#ecf0f1"
+    }
+  ]);
+
+var photoshop = new Chart(document.getElementById("photoshop").getContext("2d"))
+  .Doughnut([
+    {
+      value: 70,
+      color:"#1abc9c"
+    },
+    {
+      value : 30,
+      color : "#ecf0f1"
+    }
+  ]);
+
+var illustrator = new Chart(document.getElementById("illustrator").getContext("2d"))
+  .Doughnut([
+    {
+      value: 50,
+      color:"#1abc9c"
+    },
+    {
+      value : 50,
+      color : "#ecf0f1"
+    }
+  ]);
